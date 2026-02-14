0d2981ec4b11e133eb71726602efc2bd8e7cee25
Samarth0325 <samarthdeshmukhe3@gmail.com>
2026-02-14 15:15:32 +0530

    we create a request in views.py by chaning http request to render request and call index.html file.

--- Authentication/views.py
+++ Authentication/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello I am working")
+    return render(request, "Authentication/index.html")
 
 def signup(request):
     return render(request, "Authentication/signup.html")
