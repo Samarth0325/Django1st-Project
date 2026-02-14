bf6f2520e4f29047cadc03ccfc4d574b56715039
Samarth0325 <samarthdeshmukhe3@gmail.com>
2026-02-14 16:13:59 +0530

    I was create a signup form and add the views for signup button.

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, "Authentication/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username=request.POST["username"]
+        fname=request.POST["fname"]
+        lname=request.POST["lname"]
+        email=request.POST["email"]
+        password=request.POST["password"]
+        password2=request.POST["password2"]
+
+
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name=fname
+        myuser.last_name_name=lname
+
+        myuser.save()
+
+        messages.success(request, "your account has been created successfully.")
+    
+        return redirect('signin')
+
+
+
+
     return render(request, "Authentication/signup.html")
+
 
 def signin(request):
     return render(request, "Authentication/signin.html")
